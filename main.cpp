1687bafe82e27b51b885e1de3294b9424410d7dd
Franciszek Marszałek <f.f.marsz@gmail.com>
2026-07-22 23:56:20 +0200

    Add struct Item

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 int main() {
 
+    struct Item {
+        std::string sku{""};
+        std::string name{""};
+        std::string producent{""};
+        std::string part_number{""};
+        std::string footprint{""};
+        int min_stock_alert{};
+    };
 
     return 0;
 }
