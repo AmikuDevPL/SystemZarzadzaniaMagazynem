e4dd61073ccf8db1d7d254273cc9a8e2285a3f6c
AmikuDevPL <franciszek.marszalek@zst.info.pl>
2026-08-02 00:13:42 +0200

    feat: add new method addDrawer inside Magazine class, new method addContainers (not ready yet), new addItem method inside System manager class

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <optional>
 
 int main() {
 
     class SystemManager {
-    private:
-    struct Item {
-        std::string sku;
-        std::string name;
-        std::string manufacturer;
-        std::string part_number;
-        std::string footprint;
-        int min_stock_alert{0};
-    };
+        private:
+        struct Item {
+            std::string sku;
+            std::string name;
+            std::string manufacturer;
+            std::string part_number;
+            std::string footprint;
+            int min_stock_alert{0};
+        };
 
-    std::vector <Item> itemsCatalogue{};
+        std::vector <Item> itemsCatalogue{};
 
-    public:
-    int getItemsInCatalogueQuantity() {
-        return itemsCatalogue.size();
-    };
+        public:
+        int getItemsInCatalogueQuantity() {
+            return itemsCatalogue.size();
+        };
 
-    Item getItemBySku(sku) {
+        std::optional<Item> getItemBySku(std::string sku) {
+            for (auto& i : itemsCatalogue) {
+                if (i.sku == sku) {
+                    return i;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
 
-    }
+        std::optional<Item> getItemByName(std::string name) {
+            for (auto& i : itemsCatalogue) {
+                if (i.name == name) {
+                    return i;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        void addItem(std::string sku, std::string name, std::string manufacturer, std::string part_number, std::string footprint, int min_stock_alert) {
+            itemsCatalogue.push_back(Item(sku, name, manufacturer, part_number, footprint, min_stock_alert));
+        }
 
     };
 
@@ -32,7 +55,9 @@ int main() {
     class Magazine {
     private:
         std::string magazine_name;
+
         struct Drawer {
+            int drawer_id{0};
             int small_containers_quantity{0};
             int row{0};
             int column{0};
@@ -40,19 +65,33 @@ int main() {
                 int row{0};
                 int column{0};
                 int item_quantity{0};
-                std::string sku {};
+                std::string itemSku {};
             };
             std::vector <Small_container> small_containers{};
 
-            Drawer(const int quantity)
-                :   small_containers_quantity(quantity),
-                    small_containers(quantity)
-            {}
         };
+
+        std::vector <Drawer> drawers {};
 
     public:
         Magazine(std::string magazine_name) {
             this->magazine_name = magazine_name;
+        }
+
+        void addDrawer(int drawer_id, int small_containers_quantity, int row, int column) {
+            drawers.push_back(Drawer(drawer_id, small_containers_quantity, row, column));
+        }
+
+        void addContainers(int rows, int columns, int drawer_id) {
+            for (auto& i : drawers) {
+                if (i.drawer_id == drawer_id) {
+                    for (size_t j = 0; j < columns; j++) {
+                        for (size_t k = 0; k < rows; k++) {
+                            i.small_containers.push_back();
+                        }
+                    }
+                }
+            }
         }
     };
 
